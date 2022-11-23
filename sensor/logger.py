1d1f3a61d71396180930bd0a5873e6165e41d09c
Shivanand Ekatpure <109525315+shrchrds@users.noreply.github.com>
2022-11-23 19:26:19 +0530

    Update logger.py

--- sensor/logger.py
+++ sensor/logger.py
@@ -11,7 +11,6 @@ os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
